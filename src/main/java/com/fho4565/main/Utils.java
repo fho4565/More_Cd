d107bc8a098860ba2f2dacf2bb92d9a720cd7746
fho4565 <qazplmylg@qq.com>
2023-01-07 14:58:39 +0800

    new

--- src/main/java/com/fho4565/main/Utils.java
+++ src/main/java/com/fho4565/main/Utils.java
@@ -4,13 +4,10 @@ import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.world.scores.Objective;
 import net.minecraft.world.scores.Score;
 import net.minecraft.world.scores.Scoreboard;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 
 public class Utils {
     public static final String MODID = "more_cd";
-    public static final Logger LOGGER = LogManager.getLogger(Utils.class);
     /** @param playerName 玩家名字
      * @param objective 要获取的计分板名字*/
     public static int getScore(CommandSourceStack commandSourceStack, String playerName, Objective objective){
